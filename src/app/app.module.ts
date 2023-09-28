d3c64c0439ca40bf2ea6d38dd187214244da1db6
Deybison26 <deybison2026@gmail.com>
2023-09-27 22:03:43 -0500

    final hoja de vida

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NabvarComponent } from './components/nabvar/nabvar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AvengersComponent } from './components/avengers/avengers.component';
+import { CaruselComponent } from './components/carusel/carusel.component';
+import { HomeComponent } from './components/home/home.component';
+
+//RUTAS
+import { APP_ROUTES } from './app.routes';
+import { HojaDeVidaComponent } from './components/hoja-de-vida/hoja-de-vida.component';
+
+//SERVICIOS
+
+
+//BASE DE DATOS
+@NgModule({
+  declarations: [
+    AppComponent,
+    NabvarComponent,
+    FooterComponent,
+    AvengersComponent,
+    CaruselComponent,
+    HomeComponent,
+    HojaDeVidaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    APP_ROUTES
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./components/home/home.component";
+import { AvengersComponent } from "./components/avengers/avengers.component";
+import { CaruselComponent } from "./components/carusel/carusel.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { HojaDeVidaComponent } from "./components/hoja-de-vida/hoja-de-vida.component";
+
+const APP_ROUTING: Routes=[
+  {path: 'home', component:HomeComponent},
+  {path: 'avengers', component:AvengersComponent},
+  {path: 'carusel', component:CaruselComponent},
+  {path: 'footer', component:FooterComponent},
+  {path: 'HojaDeVida', component:HojaDeVidaComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'home'},
+];
+
+export const APP_ROUTES = RouterModule.forRoot(APP_ROUTING);
+
